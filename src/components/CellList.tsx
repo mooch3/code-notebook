8f41c3db3320db15014f28debf81af57e552fe7d
mooch3 <62298384+mooch3@users.noreply.github.com>
2021-08-30 12:04:13 -0700

    added new editor buttons and associated styling

--- src/components/CellList.tsx
+++ src/components/CellList.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import CellListItem from "./CellListItem";
 import AddCell from "./AddCell";
@@ -7,10 +8,10 @@ const CellList: React.FC = () => {
     order.map((id) => data[id])
   );
   const renderedCells = cells.map((cell) => (
-    <>
+    <Fragment key={cell.id}>
       <AddCell nextCellId={cell.id} />
       <CellListItem cell={cell} key={cell.id} />
-    </>
+    </Fragment>
   ));
   return <div>
     {renderedCells}
